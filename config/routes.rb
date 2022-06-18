bcf9716b23a5825f82af60c09706e09913063c69
EC2 Default User <ec2-user@ip-172-31-36-84.ec2.internal>
2022-06-18 13:55:46 +0000

    [2022/6/18]public/sign_up zissou

--- config/routes.rb
+++ config/routes.rb
@@ -1,35 +1,35 @@
 Rails.application.routes.draw do
  
   namespace :admin do
-  PATCH 'order_details/:id' => 'order_details#update'
+  #patch 'order_details/:id' => 'order_details#update'
   end
  
   namespace :admin do
-  PATCH 'orders/:id' => 'public/orders#orders'
+  patch 'orders/:id' => 'public/orders#orders'
   end
  
   namespace :admin do
-    get 'orders/:id' => 'orders#show'
+    get 'orders/show' => 'orders#show'
   end
   namespace :admin do
     get 'customers' => 'customers#index'
-    get 'customers/:id' => 'customers/show'
-    get 'customers/:id/edit' => 'customers#edit'
-    PATCH 'custmers/:id' => 'custmers/update'
+    get 'customers/show' => 'customers/show'
+    get 'customers/edit' => 'customers#edit'
+    #patch 'custmers/:id' => 'custmers/update'
   end
   namespace :admin do
     get 'genres' => 'genres#index'
-    POST 'genres' => 'genres#create'
-    get 'genres/:id/edit' => 'genres#edit'
-    PATCH 'genres/:id' => 'genres#update'
+    post 'genres' => 'genres#create'
+    get 'genres/edit' => 'genres#edit'
+    #patch 'genres/:id' => 'genres#update'
   end
   namespace :admin do
     get 'items'
     get 'items/new'
-    POST 'items' => 'items#create'
-    get 'items/:id' => 'items#show'
-    get 'items/:id/edit' => 'items/edit'
-    PATCH 'items/:id' => 'items#update'
+    post 'items' => 'items#create'
+    get 'items/show' => 'items#show'
+    get 'items/edit' => 'items/edit'
+    #patch 'items/:id' => 'items#update'
   end
   namespace :admin do
     root to: 'homes#top'
@@ -40,32 +40,32 @@ Rails.application.routes.draw do
   get 'homes/about' => 'public/homes#about'
   
   get '/items' => 'public/items#index'
-  get '/items/:id' => 'public/items#show'
+  get '/items/show' => 'public/items#show'
   
   get 'customers/mypage' => 'public/customers#show'
   get 'customers/edit' => 'public/customers#edit'
-  PATCH 'customers' => 'public/customers#update'
+  patch 'customers' => 'public/customers#update'
   get 'customers/unsubscribe' => 'public/customers#unsubscribe'
-  PATCH 'customers/withdrawal' => 'public/customers#withdrawal'
+  patch 'customers/withdrawal' => 'public/customers#withdrawal'
   
-  get 'cart_items' => 'public/cort_items#index'
-  PATCH 'cart_items/:id' => 'public/cort_items#update'
-  DELETE 'cart_items/:id' => 'public/cort_items#destroy'
-  DELETE 'cart_items/destroy_all' => 'public/cort_items#destroy_all'
-  POST 'cart_items' => 'public/cort_items#create'
+  get 'cart_items' => 'public/cart_items#index'
+  #patch 'cart_items/:id' => 'public/cort_items#update'
+  #delete 'cart_items/:id' => 'public/cort_items#destroy'
+  #delete 'cart_items/destroy_all' => 'public/cort_items#destroy_all'
+  post 'cart_items' => 'public/cort_items#create'
   
   get 'orders/new' => 'public/orders#new'
-  POST 'orders/confirm' => 'public/orders#confirm'
+  post 'orders/confirm' => 'public/orders#confirm'
   get 'orders/complete' => 'public/orders#complete'
-  POST 'orders/' => 'public/orders#create'
+  post 'orders/' => 'public/orders#create'
   get 'orders/index' => 'public/orders#index'
-  get 'orders/:id' => 'public/orders#show'
+  get 'orders/show' => 'public/orders#show'
   
   get 'addresses/index' => 'public/addresses#index'
-  get 'addresses/:id/edit' => 'public/addresses#edit'
-  POST 'addresses' => 'public/addresses#create'
-  PATCH 'addresses/:id' => 'public/addresses#update'
-  DELETE 'addresses/:id' => 'public/addresses#destroy'
+  get 'addresses/edit' => 'public/addresses#edit'
+  post 'addresses' => 'public/addresses#create'
+  #patch 'addresses/:id' => 'public/addresses#update'
+  #delete 'addresses/:id' => 'public/addresses#destroy'
   
   devise_for :customers, controllers: {
   sessions:      'public/customers/sessions',
